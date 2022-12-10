0363f0a8f08fd5d8e999112955bfb31e6e59b270
Artem S <asaienko@qualium-systems.com.ua>
2022-12-10 11:24:46 +0200

    Fix CSV format

--- src/app.tsx
+++ src/app.tsx
@@ -13,8 +13,16 @@ export const App = () => {
         /* temporary cors proxy */ 'https://api.codetabs.com/v1/proxy/?quest=https://docs.google.com/spreadsheets/d/19Qfp3aR8vXuiNNWkob2ZFHrYILQDdEQ_OY10HFuU5TU/gviz/tq?tqx=out:csv&sheet=CMS%20READY',
       )
       .then((res) => {
+        const csvArray = res.data.split('\n');
+        console.log(csvArray);
+        const csvString = [
+          'ID,DATE,IMG,QUOTE,NAME,EXP,BIO,SEX,PEP VERIFIED,SPECTRE0,CAT0,AFFILIATION,LINK0,ORIGIN,REGION,BEING,DIASPORA,LINK1,POS,RIGHT,LEFT,RADICAL,EQ,MR,CAT1,SP1,WR,NA,CAT2,SP2,FR,AT,CAT3,SP3,PG,TR,CAT4,SP4',
+          ...csvArray.slice(1),
+        ].join('\n');
+
+        console.log(csvString);
         csv()
-          .fromString(res.data)
+          .fromString(csvString)
           .then((data) =>
             setTableData(
               data.map((item) => {
